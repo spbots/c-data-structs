ac810425d4fdd7bbbfc42d5b18709506991b8d0d
Stephan Bots <stephan+git@stephanbots.com>
2019-02-27 20:46:30 +0100

    Remove scaffolding tests

--- src/bst.c
+++ src/bst.c
@@ -30,52 +30,6 @@ static const node_t* find_node(const node_t* root, const int key) {
     assert(0);
 }
 
-// ----------------
-// ------impl------
-// ----------------
-node_t* bst_alloc(const int root_key) {
-    node_t* new_node = malloc(sizeof(node_t));
-    new_node->key.data = root_key;
-    new_node->key.is_valid = 1;
-    new_node->l = NULL;
-    new_node->r = NULL;
-    return new_node;
-}
-
-void bst_free(node_t* root) {
-    if (root) {
-        bst_free(root->l);
-        bst_free(root->r);
-        free(root);
-    }
-}
-
-int bst_has_value(const node_t* root, const int key) {
-    return !!find_node(root, key);
-}
-
-void bst_insert(node_t* root, const int key) {
-    assert(root && root->key.data != key);
-    if (key < root->key.data) {
-        if (root->l) {
-            bst_insert(root->l, key);
-        } else {
-            root->l = bst_alloc(key);
-        }
-    } else {
-        if (root->r) {
-            bst_insert(root->r, key);
-        } else {
-            root->r = bst_alloc(key);
-        }
-    }
-}
-
-void bst_remove(node_t* root, const int key) {
-    node_t* n = (node_t*)find_node(root, key);
-    if (n) { n->key.is_valid = 0; }
-}
-
 /*
 incoming node is node d.
 returns new root node b.
@@ -136,39 +90,55 @@ static void left_rotate(node_t* node) {
     }
 }
 
-// create a "backbone" (no left children)
-static void make_backbone(node_t* node) {
-    while(node) {
-        while(node->l) {
-            right_rotate(node);
-        }
-        node = node->r;
+// ----------------
+// ------impl------
+// ----------------
+node_t* bst_alloc(const int root_key) {
+    node_t* new_node = malloc(sizeof(node_t));
+    new_node->key.data = root_key;
+    new_node->key.is_valid = 1;
+    new_node->l = NULL;
+    new_node->r = NULL;
+    return new_node;
+}
+
+void bst_free(node_t* root) {
+    if (root) {
+        bst_free(root->l);
+        bst_free(root->r);
+        free(root);
     }
 }
 
-// remove and free any nodes with null keys.
-// ... can change the root node, which is why it returns.
-// ... very branchy (2 per node), yuck.
-// ... could be part of building the backbone?
-static node_t* remove_invalid_nodes_from_backbone(node_t* root) {
-    node_t* parent = NULL;
-    node_t* child = root;
-    root = NULL;
+int bst_has_value(const node_t* root, const int key) {
+    const node_t* n = find_node(root, key);
+    return n && n->key.is_valid;
+}
 
-    while (child) {
-        assert(!child->l);
-        if (!child->key.is_valid) {
-            node_t* node_to_free = child;
-            child = child->r;
-            if (parent) { parent->r = child; }
-            free(node_to_free);
+void bst_insert(node_t* root, const int key) {
+    assert(root);
+    if (key < root->key.data) {
+        if (root->l) {
+            bst_insert(root->l, key);
         } else {
-            parent = child;
-            child = child->r;
-            if (!root) { root = parent; }
+            root->l = bst_alloc(key);
         }
+    } else if (key > root->key.data) {
+        if (root->r) {
+            bst_insert(root->r, key);
+        } else {
+            root->r = bst_alloc(key);
+        }
+    } else {
+        // adding a node that was removed before rebalancing
+        assert(!root->key.is_valid);
+        root->key.is_valid = 1;
     }
-    return root;
+}
+
+void bst_remove(node_t* root, const int key) {
+    node_t* n = (node_t*)find_node(root, key);
+    if (n) { n->key.is_valid = 0; }
 }
 
 // return the new root
@@ -176,8 +146,39 @@ node_t* bst_balance_a(node_t* root) {
     // Day-Stout-Warren algorithm, see
     // http://courses.cs.vt.edu/
     //      cs2604/spring05/mcpherson/note/BalancingTrees.pdf
-    make_backbone(root);
-    root = remove_invalid_nodes_from_backbone(root);
+
+    { // make backbone
+        node_t* node = root;
+        while(node) {
+            while(node->l) {
+                right_rotate(node);
+            }
+            node = node->r;
+        }
+    }
+
+    { /* remove and free any nodes with null keys.
+        ... can change the root node, which is why it returns.
+        ... very branchy (2 per node), yuck.
+        ... could be part of building the backbone? */
+        node_t* parent = NULL;
+        node_t* child = root;
+        root = NULL;
+
+        while (child) {
+            assert(!child->l);
+            if (!child->key.is_valid) {
+                node_t* node_to_free = child;
+                child = child->r;
+                if (parent) { parent->r = child; }
+                free(node_to_free);
+            } else {
+                parent = child;
+                child = child->r;
+                if (!root) { root = parent; }
+            }
+        }
+    }
 
     const int n_nodes = num_nodes(root);
     int m = (int)pow(2.0, floor(log2(n_nodes + 1.0))) - 1;
@@ -212,60 +213,9 @@ static node_t* make_alnum_tree(const char* str) {
     node_t* root = bst_alloc(str[0]);
     for (int i = 1; str[i] != '\0'; ++i) {
         bst_insert(root, str[i]);
+        assert(bst_has_value(root, str[i]));
     }
     return root;
-}
-
-static void test_rotate() {
-    node_t* root = make_alnum_tree("dbeac");
-
-    right_rotate(root);
-    assert(root->key.data == 'b');
-    assert(root->l->key.data == 'a');
-    assert(root->r->key.data == 'd');
-    assert(root->r->l->key.data == 'c');
-    assert(root->r->r->key.data == 'e');
-
-    left_rotate(root);
-    assert(root->key.data == 'd');
-    assert(root->l->key.data == 'b');
-    assert(root->r->key.data == 'e');
-    assert(root->l->l->key.data == 'a');
-    assert(root->l->r->key.data == 'c');
-
-    bst_free(root);
-}
-
-static int backbone_node_value(node_t* node, int depth) {
-    node_t* n = node;
-    for (int i = 0; i < depth; ++i) { n = n->r; }
-    return n->key.data;
-}
-
-static void test_backbone() {
-    node_t* root = make_alnum_tree("dhbfeigjac");
-    assert(num_nodes(root) == 10);
-
-    make_backbone(root);
-    assert(root->key.data == 'a');
-    assert(root->r->key.data == 'b');
-    assert(root->r->r->key.data == 'c');
-    assert(root->r->r->r->key.data == 'd');
-    assert(root->r->r->r->r->key.data == 'e');
-
-    bst_remove(root, 'a'); assert(!find_node(root, 'a')->key.is_valid);
-    bst_remove(root, 'b'); assert(!find_node(root, 'b')->key.is_valid);
-    bst_remove(root, 'e'); assert(!find_node(root, 'e')->key.is_valid);
-    bst_remove(root, 'f'); assert(!find_node(root, 'f')->key.is_valid);
-    bst_remove(root, 'j'); assert(!find_node(root, 'j')->key.is_valid);
-    root = remove_invalid_nodes_from_backbone(root);
-    assert(backbone_node_value(root, 0) == 'c');
-    assert(backbone_node_value(root, 1) == 'd');
-    assert(backbone_node_value(root, 2) == 'g');
-    assert(backbone_node_value(root, 3) == 'h');
-    assert(backbone_node_value(root, 4) == 'i');
-
-    bst_free(root);
 }
 
 static void test_balance() {
@@ -322,7 +272,5 @@ static void test_balance() {
 }
 
 void bst_test() {
-    test_rotate();
-    test_backbone();
     test_balance();
 }
